0efcdc7c117a16758f432901ec750eb0801869ef
Kevin Yang <chgc_kevin@hotmail.com>
2015-10-18 19:28:27 +0800

    String templates

--- public/index.js
+++ public/index.js
@@ -1,36 +1,13 @@
 /*
-* Arrow scope
+* String templates
 */
-// Old
-// function old_sayHello(){
-//     var _this = this;
-// 
-//     _this.name = 'hendrik';
-// 
-//     setTimeout(function(){
-//         console.log('hello ' + _this.name + ' (old)');
-//     }, 1500);
-// }
-// 
-// old_sayHello();
-// New
-// ES6可以用`來包文字, 在字串中可以用${變數} 來表示
-function new_sayHello() {
-    var _this = this;
-    this.name = 'hendrik';
-    setTimeout(function () {
-        console.log("hello " + _this.name + " (new)");
-    }, 1500);
+// old
+function sayHello(name, surname) {
+    console.log('hello there ' + name + ' ' + surname + ', the time is now ' + new Date());
 }
-new_sayHello();
-var hendrik = {
-    name: "Hendrik",
-    sayHello: function (names) {
-        var _this = this;
-        names.forEach(function (name) {
-            console.log(_this.name + " greets " + name);
-        });
-    }
-};
-hendrik.sayHello(['frikkie']);
-// this should output hendrik greets frikkie 
+sayHello('kevin', 'Yang');
+// new way 
+function sayHello_new(name, surname) {
+    console.log("hello there " + name + " " + surname + ", \n    the time is now " + new Date());
+}
+sayHello_new('kevin', 'Yang');
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,18 @@
 /*
-* Arrow scope
+* String templates
 */
 
-// Old
-// function old_sayHello(){
-//     var _this = this;
-// 
-//     _this.name = 'hendrik';
-// 
-//     setTimeout(function(){
-//         console.log('hello ' + _this.name + ' (old)');
-//     }, 1500);
-// }
-// 
-// old_sayHello();
-
-// New
-// ES6可以用`來包文字, 在字串中可以用${變數} 來表示
-function new_sayHello(){
-    this.name = 'hendrik';
-
-    setTimeout(()=>{
-        console.log(`hello ${this.name} (new)`);
-    }, 1500);
+// old
+function sayHello(name, surname){
+    console.log('hello there ' + name + ' ' + surname + ', the time is now ' + new Date());
 }
 
-new_sayHello();
+sayHello('kevin','Yang');
 
-var hendrik = {
-    this.name = "Hendrik";
-    
-    sayHello: (names) => {
-        names.forEach((name)=>{
-          console.log(`${this.name} greets ${name}`);
-        });
-    }
+// new way 
+function sayHello_new(name, surname){
+    console.log(`hello there ${name} ${surname}, 
+    the time is now ${new Date()}`);
 }
-hendrik.sayHello(['frikkie']);
-// this should output hendrik greets frikkie
+
+sayHello_new('kevin', 'Yang');
