c6588d20951442411a8e696a676e0b252e74c8b9
Kevin Yang <chgc_kevin@hotmail.com>
2015-10-18 19:00:21 +0800

    arrow function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-console.log("test");
+/*
+* Arrow functions
+*/
+// Old
+var old = function (n) {
+    return n * n;
+};
+// with Arror Functions express 1
+var new_1 = function (n) { return n * n; };
+// with arror function express 2
+var new_2 = function (n) {
+    return n * n;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("test");
+/*
+* Arrow functions
+*/
+
+// Old
+var old = function(n) {
+    return n * n;
+};
+
+// with Arror Functions express 1
+let new_1 = (n) => n * n;
+
+// with arror function express 2
+let new_2 = (n) => {
+	return n * n;
+};
+
